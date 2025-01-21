6e062e9215ec2933d0a0fca108e2db6d01d38100
= <=>
2025-01-21 14:12:34 +0000

    get request for invoices fully implemented

--- src/components/molecules/Form/Form.tsx
+++ src/components/molecules/Form/Form.tsx
@@ -37,40 +37,6 @@ interface FormData {
   total: number;
 }
 
-
-// {
-//   "id": "RT3080",
-//   "createdAt": "2021-08-18",
-//   "paymentDue": "2021-08-19",
-//   "description": "Re-branding",
-//   "paymentTerms": 1,
-//   "clientName": "Jensen Huang",
-//   "clientEmail": "jensenh@mail.com",
-//   "status": "paid",
-//   "senderAddress": {
-//     "street": "19 Union Terrace",
-//     "city": "London",
-//     "postCode": "E1 3EZ",
-//     "country": "United Kingdom"
-//   },
-//   "clientAddress": {
-//     "street": "106 Kendell Street",
-//     "city": "Sharrington",
-//     "postCode": "NR24 5WQ",
-//     "country": "United Kingdom"
-//   },
-//   "items": [
-//     {
-//       "name": "Brand Guidelines",
-//       "quantity": 1,
-//       "price": 1800.90,
-//       "total": 1800.90
-//     }
-//   ],
-//   "total": 1800.90
-// },
-
-
 function Form() {
   const dispatch = useAppDispatch();
   const isEdit = useAppSelector(state => state.pageState.isEdit);
